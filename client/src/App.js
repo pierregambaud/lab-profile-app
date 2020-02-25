da4c60c8d40b20c0d5ac33f3faa11a876e70a3ff
blablamedia <pierre@gambaud.com>
2020-02-25 20:04:17 +0100

    iteration 2: done

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hey!</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>IronProfile</h1>
+        <Link to="/signup">Signup</Link> - <Link to="/login">Login</Link>
+
+        <Switch>
+          <Route exact path="/signup" component={Signup}></Route>
+          <Route exact path="/login" component={Login}></Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
